3c990cfeedefb1a659864fa8dacb404dda332452
Ismatulla Mansurov <ismatullamansurov@gmail.com>
2024-12-15 16:37:00 -0700

    chore: update .gitignore

--- scripts/cleanup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Find and remove containers older than 1 hour
-docker ps -a | grep 'container-' | awk '{print $1}' | xargs -r docker rm -f
-
-# Clean up temporary directories
-rm -rf tmp/*
